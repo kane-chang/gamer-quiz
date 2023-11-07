49817673315cb025f5541618d47be3d5795adf64
Kane Chang <kane.chang.fl@gmail.com>
2023-11-07 20:49:06 +0000

    added TODO comments to js

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// TODO make a timer that countdowns when start quiz button is clicked
+
+// TODO add questions that display when quiz button is clicked
+
+// TODO add clickable answers
+
+// TODO function to check correct answer - reduce timer if wrong
+
+// TODO game ends when all questions answered / timer reach 0
+
+// TODO end screen with score (time left) and input initials
