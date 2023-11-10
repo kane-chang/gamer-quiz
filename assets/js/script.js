b35921e29d27e777454005cbee29413b357f192f
Kane Chang <kane.chang.fl@gmail.com>
2023-11-10 20:27:15 +0000

    added displayEndScreen() when timer or questions run out. Restructured code and added comments for readability

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,13 @@
-// TODO quiz questions:
-
+// TODO variables and quiz questions:
 // variables referencing html selectors
 var startScreen = document.querySelector("#start-screen");
 var questionDiv = document.querySelector("#questions");
 var questionTitle = document.querySelector("#question-title");
 var choicesList = document.querySelector("#choices");
+var endScreen = document.querySelector("#end-screen");
+var finalScore = document.querySelector("#final-score");
+var timeEl = document.querySelector('#time');
+var startButton = document.querySelector("#start");
 
 // quiz questions and answers
 var questionArr = ["What was Mario's original name?",
@@ -31,89 +34,83 @@ var correctAnswerArr = [
 ];
 
 
-var questionNumTracker = 0;
+var questionNumTracker = 0;  // Keeps track of which question number user is currently on
+var timeLeft = 60;  // Total time left, starting from 60 seconds
 
-
+// TODO function - to display questions and answer options
 function displayQuestion() {
-  questionTitle.textContent = questionArr[questionNumTracker]; // Set question title
+  questionTitle.textContent = questionArr[questionNumTracker];  // Set question title in html
 
-  while (choicesList.firstChild) {
+  while (choicesList.firstChild) {  // Remove previous answer options, if any
     choicesList.removeChild(choicesList.firstChild);
-  }
+  };
 
-  for (let j = 0; j < 4; j++) {
+  for (let j = 0; j < 4; j++) {  // Create buttons for answer options for each question
     var choiceEl = document.createElement("button");
     choiceEl.textContent = answerArr[questionNumTracker][j];
     choicesList.appendChild(choiceEl);
   };
 
-
-  startScreen.removeAttribute('class:"start"');
+  startScreen.removeAttribute('class:"start"');  // Hides start screen
   startScreen.setAttribute("class", "hide");
 
-  questionDiv.removeAttribute('class:"hide"');
+  questionDiv.removeAttribute('class:"hide"');  // Displays questions screen
   questionDiv.setAttribute("class", "start");
 };
 
 
-  // questionTitle.textContent = questionList.questionOne.title
-  // for (let i = 0; i < questionList.questionOne.choices.length; i++) {
-  //   var choiceEl = document.createElement("button")
-  //   choiceEl.textContent = questionList.questionOne.choices[i]
-  //   choicesList.appendChild(choiceEl)
-  // }
-
-
-// TODO make a timer that countdowns when start quiz button is clicked
-
-var timeEl = document.getElementById('time')
-var timeLeft = 60;
-
+// TODO function - timer that countdowns when start quiz button is clicked
 function countdown() {
-
   var timeInterval = setInterval(function () {
-    timeEl.textContent = timeLeft
-    timeLeft--;
-    if (timeLeft < 0) {
-      timeEl.textContent = "0"
-      clearInterval(timeInterval)
-      // trigger function to display end screen
-    }
+    timeEl.textContent = timeLeft  // Sets time element to reflect total time left
+    timeLeft--;  // Deducts 1 from total time left every 1000ms
+    if (timeLeft < 1 | questionNumTracker >= questionArr.length) {  // If time/questions run out, stops timer
+      timeEl.textContent = timeLeft;
+      clearInterval(timeInterval);
+      displayEndScreen();  // Triggers end screen
+    };
   }, 1000);
-}
+};
 
-
-// TODO add questions that display when quiz button is clicked
-var startButton = document.querySelector("#start");
-startButton.addEventListener("click", countdown);
-startButton.addEventListener("click", displayQuestion);
-
-
-// TODO add clickable answers
-choicesList.addEventListener("click", answerChecker);
-
-// TODO function to check correct answer - reduce timer if wrong
+// TODO function - to check correct answer - reduce timer if wrong
 function answerChecker(event) {
-  if (event.target.matches("button")) {
-    selectedAnswer = event.target;
-    console.log(selectedAnswer.textContent);
+  if (event.target.matches("button")) {  // Event delegation to only child button elements
+    selectedAnswer = event.target.textContent;  // Stores selected answer 
+    console.log(selectedAnswer);
     console.log(correctAnswerArr[questionNumTracker]);
     
-    if (selectedAnswer.textContent == correctAnswerArr[questionNumTracker]) {
+    if (selectedAnswer == correctAnswerArr[questionNumTracker]) {  // Checks if selected answer is correct
       console.log("yes!");
-    } else {
+    } else {  
       console.log("no!");
-      timeLeft -= 10
+      timeLeft -= 10  // Reduces time for wrong answer
     };
-    questionNumTracker += 1;
-    displayQuestion()
+
+    questionNumTracker += 1;  // Moves tracker to next question
+    if (questionNumTracker >= questionArr.length) {  // Checks for end of quiz
+      console.log("out of questions");
+      displayEndScreen()
+    } else {
+      displayQuestion()
+    };
   } 
-}
+};
 
-// TODO game ends when all questions answered / timer reach 0
 
-// if (timeLeft < 1 | questionNumTracker == questionArr.length - 1) {
-  
-// }
+// TODO function - display end screen with score (time left) and input initials
+function displayEndScreen() {
+  console.log(timeLeft);
+  finalScore.textContent = timeLeft  // Updates final score with time left
 
-// TODO end screen with score (time left) and input initials
+  questionDiv.removeAttribute('class:"start"');  // Hides questions
+  questionDiv.setAttribute("class", "hide");
+
+  endScreen.removeAttribute('class:"hide"');  // Displays end screen
+  endScreen.setAttribute("class", "start");
+};
+
+
+// TODO event listeners - start button and answer buttons
+startButton.addEventListener("click", countdown);
+startButton.addEventListener("click", displayQuestion);
+choicesList.addEventListener("click", answerChecker);
