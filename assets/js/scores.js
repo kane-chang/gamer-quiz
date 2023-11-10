b0a9da6b5f88872b14e16d8a99183552cc14d001
Kane Chang <kane.chang.fl@gmail.com>
2023-11-10 22:29:56 +0000

    bugfix clearHighscore() not displaying cleared highscore immediately

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -4,13 +4,24 @@ var clearButton = document.querySelector("#clear")
 
 
 // TODO function - retrieve and display highscore from local storage
-var highscores = JSON.parse(localStorage.getItem("scores"))
-// console.log(highscores);
-for (let i = 0; i < highscores.length; i++) {
-    var newScorer = document.createElement("li")
-    newScorer.textContent = highscores[i]
-    highscoreList.appendChild(newScorer)
+function displayHighscores() {
+    while (highscoreList.firstChild) {
+        highscoreList.removeChild(highscoreList.firstChild)
+    };
+
+    if (localStorage.getItem("scores") != null) {
+        var highscores = JSON.parse(localStorage.getItem("scores"))
+        // console.log(highscores);
+
+        for (let i = 0; i < highscores.length; i++) {
+            var newScorer = document.createElement("li")
+            newScorer.textContent = highscores[i]
+            highscoreList.appendChild(newScorer)
+        };
+    };
 };
+
+displayHighscores()
 
 
 // TODO function - clear highscore button
@@ -18,4 +29,5 @@ clearButton.addEventListener("click", clearHighscore)
 
 function clearHighscore() {
     localStorage.clear()
+    displayHighscores()
 }
